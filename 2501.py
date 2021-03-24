7291311e24d0d47219c251e04c04bd26ae1bb697
jaewon1676 <jaewon1676@daum.net>
2021-03-24 23:10:03 +0900

    test

--- 2501.py
+++ 2501.py
@@ -1,2 +1,16 @@
-2501.py
-print('1')
+#백준 2501 약수 구하기
+
+n, k = map(int,input().split())
+
+num_list=[]
+
+for i in range(1,n+1):
+    if n%i==0:
+        num_list.append(i)
+
+if len(num_list)>=k:
+    print(num_list[k-1])
+else:
+    print(0)
+
+
