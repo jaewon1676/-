9ca15cb3e695d1dec0ce081dd4ef22cf9479a944
jaewon1676 <jaewon1676@daum.net>
2021-03-25 00:48:34 +0900

    약수 구하기

--- 2051.py
+++ 2051.py
@@ -1,13 +1,11 @@
 # 백준 2051 약수 구하기
 # 두 개의 자연수 N과 K가 주어졌을 때, 
 # N의 약수들 중 K번째로 작은 수를 출력하는 프로그램을 작성하시오.
-
-n, k=map(int,input().split())
-
+n, k=map(int,input().split()) # int형으로 두 개 입력 받는다.
 num_list=[]
 
 for i in range(1,n+1):
-    if n%i==0:  # n을 i로 나누어 나머지가 0이 되면
+    if n%i==0:             # n을 i로 나누어 나머지가 0이 되면
         num_list.append(i) # i를 추가
 
 if len(num_list)>=k:
---
+++ test.py
@@ -0,0 +1,4 @@
+a = [1.2, 2.5, 3.7, 4.6]
+
+for i in range(len(a)):
+    a[i]=int(a[i])
