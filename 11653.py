6b120281d2d9009dff4363454b5197ca5a5ce2dd
jaewon1676 <jaewon1676@daum.net>
2021-03-25 16:05:40 +0900

    소인수분해

--- 11653.py
+++ 11653.py
@@ -5,8 +5,8 @@ n = int(input())
 i = 2
 
 while n != 1 :
-    if n%i == 0:
+    if n%i == 0: # 2로 나누어지면 진행
         n = n//i
         print(i)
-    else:
+    else:        # 2로 나누어지지 않으면 i에 1 증가
         i+=1
