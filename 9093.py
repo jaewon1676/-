a290a96605f81023d67fdfc930ece2ff9ab8f5a2
jaewon1676 <jaewon1676@daum.net>
2021-03-30 17:51:28 +0900

    단어 뒤집기

--- 9093.py
+++ 9093.py
@@ -2,12 +2,12 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input()) # 입력 할 숫자 입력
-data = [list(input().split()) for i in range(n)] 
-# 문자 입력 하고 list형으로 data변수에 저장한다.
-for i in range(n):
-    for j in data[i]: # data 변수의 문자를 j 변수에 옮긴다
-        print(''.join(reversed(list(j))), end=" ")
-        # join 함수를 이용해 받은 문자를 더해 문자열로 연결하고
-        # reversed 함수를 이용해 문자열을 뒤집는다.
-    print()
+t = int(input()) # 입력 할 숫자 입력
+
+for _ in range(t):
+    n = list(map(str,input()))
+    if n.count('(') == n.count(')'):
+        print('YES')
+    else:
+        print('NO')
+        
