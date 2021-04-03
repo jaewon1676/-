525a5fbab21a34450b010490fa4450044f20b3fa
jaewon1676 <jaewon1676@daum.net>
2021-04-03 16:43:04 +0900

    요세푸스 문제

--- 1158.py
+++ 1158.py
@@ -12,12 +12,9 @@ while True:
     result.append(people.pop(i)) # 1번째 (2) = 3
     if not people:            # people 비어있으면 종료
         break
-    i  = (i+k-1) % len(people)  
+    i  = (i+k-1) % len(people) 
     # 2번째 4 % 6 = (4) = 6, 3번째 6 % 5 = (1) = 2
     # 4번째 3 % 4 = (3) = 7, 5번째 5 % 3 = (2) = 5
     # 6번째 4 % 2 = (0) = 1, 6번째 2 % 1 = (0) = 4
-    print(result)
-
-
 print('<'+', '.join(map(str,result)) + '>')
 
