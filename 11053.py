6028eb9385909cd9cb1a80bcfd020762c0489a54
jaewon1676 <jaewon1676@daum.net>
2021-04-06 21:15:19 +0900

    가장 긴 증가하는 부분 수열

---
+++ 11053.py
@@ -0,0 +1,15 @@
+# 백준 11053 가장 긴 증가하는 부분 수열
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+d=[1] * n
+
+for i in range(1,n):
+  for j in range(i):         
+    if a[j] < a[i]:            
+      d[i] = max(d[i],d[j]+1)
+
+print(max(d))
