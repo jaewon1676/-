fc9363a3b7e8a0869c22a2536187b4f0e43b8325
jaewon1676 <jaewon1676@daum.net>
2021-04-06 11:56:22 +0900

    1, 2, 3 더하기

--- 9095.py
+++ 9095.py
@@ -1,17 +1,16 @@
 # 백준 9095 1, 2, 3 더하기
 # 정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
+# 정수 n이 주어진다. n은 양수이며 11보다 작다.
 import sys
 input = sys.stdin.readline
-input_list = []
-n = int(input())               # 정수 n 입력
 
-for i in range(n):
-    input_list.append(int(input()))        
+dp = [0] * 12
+dp[1], dp[2], dp[3] = 1, 2, 4
 
-dp = [1, 2, 4]   # 타일 초기화 및 생성.
-
-for i in range(3, max(input_list)):
-    dp.append(dp[i-1]+dp[i-2]+dp[i-3])
-
-for i in input_list:
-    print(dp[i-1])
+for i in range(4,12):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    
+n = int(input())
+for _ in range(n):
+    input_n = int(input())
+    print(dp[input_n])
