71022df016462b8b141c10971a0f4fb912efa002
jaewon1676 <jaewon1676@daum.net>
2021-04-07 23:11:25 +0900

    ㅊ

---
+++ 1912.py
@@ -0,0 +1,14 @@
+# 백준 1912 연속합
+# DP
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+sum = [a[0]]
+for i in range(n-1): # 0부터 n-1까지 반복
+    sum.append(max(sum[i] + a[i+1], a[i+1]))
+                  #sum[0] + a[1],   a[1] 중 큰 걸 sum 뒤에 추가
+                  #sum[1] + a[2],   a[2] 중 큰 걸 sum 뒤에 추가
+                  #sum[2] + a[3],   a[3] 중 큰 걸 sum 뒤에 추가
+print(max(sum))
