b3c080f66c485a7efe627d642c9b7898787ab3b0
jaewon1676 <jaewon1676@daum.net>
2021-04-08 00:53:05 +0900

    연속합

--- 1912.py
+++ 1912.py
@@ -3,12 +3,13 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-a = list(map(int, input().split()))
-sum = [a[0]]
+n = int(input())   # 입력 받을 정수 개수
+a = list(map(int, input().split()))   # n개의 정수 입력
+sum = [a[0]]       # 2차원 배열
 for i in range(n-1): # 0부터 n-1까지 반복
     sum.append(max(sum[i] + a[i+1], a[i+1]))
                   #sum[0] + a[1],   a[1] 중 큰 걸 sum 뒤에 추가
                   #sum[1] + a[2],   a[2] 중 큰 걸 sum 뒤에 추가
                   #sum[2] + a[3],   a[3] 중 큰 걸 sum 뒤에 추가
+    print(sum)
 print(max(sum))
