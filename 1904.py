efb9d0a1d238b9c11fac787a2bb8b73fc59f6996
jaewon1676 <jaewon1676@daum.net>
2021-04-07 12:21:12 +0900

    01 타일

---
+++ 1904.py
@@ -0,0 +1,13 @@
+# 백준 1904 01타일
+# DP
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * 1000001
+
+dp[1] = 1
+dp[2] = 2
+for i in range(3, n+1):
+    dp[i] = (dp[i-1] + dp[i-2]) % 15746
+print(dp[n])
