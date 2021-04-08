9f8fe92138c342861dbf6bc662809642b5489621
jaewon1676 <jaewon1676@daum.net>
2021-04-08 22:19:27 +0900

    타일링

---
+++ 1793.py
@@ -0,0 +1,20 @@
+# 백준 1793 타일링
+# DP
+# 테스트케이스가 주어지지 않아 try & except 구문 사용
+import sys
+input = sys.stdin.readline
+p = [0 for _ in range(251)] # 입력 n <= 250
+while(True):
+    try:
+        n = int(input())
+        p[0] = 1
+        p[1] = 1
+        p[2] = 3
+# p = [1, 1, 3]으로도 선언 가능
+        for i in range(3, 251): # or (3, n+1) 
+            p[i] = p[i-1] + (p[i-2]*2)
+            # 251까지 p[i]를 정의 해 놓는다.
+        print(p[n])
+    except:  
+        break
+        
