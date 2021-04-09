cf161e33b45aa5f6af796b8f04059d0ecb5ae901
jaewon1676 <jaewon1676@daum.net>
2021-04-09 16:58:47 +0900

    타일링

--- 1793.py
+++ 1793.py
@@ -5,16 +5,19 @@ import sys
 input = sys.stdin.readline
 p = [0 for _ in range(251)] # 입력 n <= 250
 while(True):
-    try:
+    try:    # n에 아무것도 입력 받지 않으면 break.           
         n = int(input())
         p[0] = 1
         p[1] = 1
         p[2] = 3
-# p = [1, 1, 3]으로도 선언 가능
+        # p = [1, 1, 3]으로도 선언 가능
         for i in range(3, 251): # or (3, n+1) 
             p[i] = p[i-1] + (p[i-2]*2)
             # 251까지 p[i]를 정의 해 놓는다.
         print(p[n])
-    except:  
+    except:
         break
-        
+# try:
+#     실행할 코드
+# except:
+#     예외가 발생했을 때 처리하는 코드
