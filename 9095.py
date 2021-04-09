8d00a5923cacf247df4971423528ae7272194292
jaewon1676 <jaewon1676@daum.net>
2021-04-09 15:56:54 +0900

    1, 2, 3 더하기

--- 9095.py
+++ 9095.py
@@ -9,8 +9,12 @@ dp[1], dp[2], dp[3] = 1, 2, 4
 
 for i in range(4,12):
     dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-    
+# for _ in range(4,12): 
+#    dp.append(sum(dp[-3:]))
+
 n = int(input())
 for _ in range(n):
     input_n = int(input())
     print(dp[input_n])
+# for _ in range(int(input())): 
+#    print(dp[int(input())])
