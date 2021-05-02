3f53dfc0f72c0e27f27c461d4e5979b9b99b453f
jaewon1676 <jaewon1676@daum.net>
2021-05-02 23:39:14 +0900

    분해합

---
+++ 2231.py
@@ -0,0 +1,14 @@
+# 백준 2231 분해합
+num = int(input())
+answer = 0
+
+for i in range(1, num+1):
+    coef_num_list = list(map(int, str(i)))
+    answer = i + sum(coef_num_list)
+    
+    if answer == num:
+        print(i)
+        break
+
+    if i == num:
+        print(0)
