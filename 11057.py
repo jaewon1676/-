4110e04d1b36fe9ca5a05ca5685646d6d08012cd
jaewon1676 <jaewon1676@daum.net>
2021-07-18 15:06:19 +0900

    오르막 수

---
+++ 11057.py
@@ -0,0 +1,21 @@
+# 백준 11057 오르막 수
+# DP
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+matrix = [[0]*10 for i in range(n)]
+matrix[0] = [1 for i in range(10)]
+
+for i in range(1, n):
+    sum = 0
+    for j in range(10):
+        for k in range(j+1):
+            matrix[i][j] += matrix[i-1][k]
+            sum += matrix[i-1][k]
+
+if n == 1:
+    print(10)
+else:
+    print(sum % 10007)
