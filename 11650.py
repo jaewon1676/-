ef606e62c86a17fbc6336d581fe935a774291c48
jaewon1676 <jaewon1676@daum.net>
2021-08-03 16:40:52 +0900

    좌표 정렬하기

---
+++ 11650.py
@@ -0,0 +1,17 @@
+# 좌표 정렬하기
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = []
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    m.append((a, b))
+
+# x좌표 오름차순 우선정렬, y좌표 오름차순정렬
+m.sort(key = lambda a:(a[0], a[1]))
+
+for i in range(n):
+        print(m[i][0], m[i][1])
